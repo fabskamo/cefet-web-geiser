f9bb8dde8389e00fcac99cb79e0e8c361c4d2481
Fabiana <fabiana.kamo@hotmail.com>
2022-01-25 20:22:36 -0300

    Exercise 2

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 // importação de dependência(s)
 import express from 'express'
+import hbs from 'hbs'
+import fs from 'fs'
 
 
 // variáveis globais deste módulo
@@ -10,7 +12,8 @@ const app = express()
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
-
+db.jogadores = JSON.parse(fs.readFileSync('server/data/jogadores.json'))
+db.jogosPorJogadores = JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json'))
 
 
 
@@ -18,6 +21,8 @@ const app = express()
 //app.set('view engine', '???qual-templating-engine???');
 //app.set('views', '???caminho-ate-pasta???');
 // dica: 2 linhas
+app.set('view engine', 'hbs')
+app.set('views', 'server/views')
 
 
 // EXERCÍCIO 2
@@ -25,23 +30,42 @@ const app = express()
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json (~3 linhas)
-
-
+app.get('/', (req, res) => {
+    res.render('index', db.jogadores)
+  })
+  
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
-
+app.get('/jogador/:id', (req, res) => {
+    const playerdId = req.params.id
+  
+    const player = db.jogadores.players.find(({ steamid }) => steamid === playerdId)
+    const games = db.jogosPorJogadores[playerdId]
+  
+    games.games.sort((a, b) => b.playtime_forever - a.playtime_forever)
+  
+    const calculated = {
+      nonPlayed: games.games.filter(({ playtime_forever }) => playtime_forever === 0).length,
+      favoriteGame: { ...games.games[0], playtime_forever: parseInt(games.games[0].playtime_forever / 60) },
+      mostPlayedGames: games.games.slice(0, 5).map(game => ({ ...game, playtime_forever: parseInt(game.playtime_forever / 60) }))
+    }
+  
+    console.log(calculated)
+  
+    res.render('jogador', { player, game_count: games.game_count, calculated })
+  })
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
-app.use (express.static("client/"))
+app.use (express.static('client/'))
 
 // abrir servidor na porta 3000 (constante PORT)
 // dica: 1-3 linhas de código
 app.listen(PORT, () => {
-    console.log("App listening on port 3000")
+    console.log('App listening on port 3000')
   })
